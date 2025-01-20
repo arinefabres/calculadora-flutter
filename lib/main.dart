72f1e5868186a607b37de5c3a56b572e183a814a
Christian Koziel <christiankoziel@hotmail.com>
2025-01-19 22:46:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculadoraApp());
+}
+
+class CalculadoraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CalculadoraPage(),
+    );
+  }
+}
+
+class CalculadoraPage extends StatefulWidget {
+  @override
+  _CalculadoraPageState createState() => _CalculadoraPageState();
+}
+
+class _CalculadoraPageState extends State<CalculadoraPage> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0;
+  double num2 = 0;
+  String operand = "";
+
+  void buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "*" ||
+        buttonText == "/") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+      switch (operand) {
+        case "+":
+          _output = (num1 + num2).toString();
+          break;
+        case "-":
+          _output = (num1 - num2).toString();
+          break;
+        case "*":
+          _output = (num1 * num2).toString();
+          break;
+        case "/":
+          _output = (num1 / num2).toString();
+          break;
+      }
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else {
+      _output += buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(buttonText, style: TextStyle(fontSize: 20.0)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Calculadora")),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(output, style: TextStyle(fontSize: 48.0)),
+          ),
+          Expanded(child: Divider()),
+          Column(
+            children: [
+              Row(children: ["7", "8", "9", "/"].map(buildButton).toList()),
+              Row(children: ["4", "5", "6", "*"].map(buildButton).toList()),
+              Row(children: ["1", "2", "3", "-"].map(buildButton).toList()),
+              Row(children: [".", "0", "C", "+"].map(buildButton).toList()),
+              Row(children: ["="].map(buildButton).toList()),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
